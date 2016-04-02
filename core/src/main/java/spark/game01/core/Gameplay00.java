85a1ff70086498c44932e5d496e8f3a855b38a38
Phongsart P. Phatthaisong <absolute.park@gmail.com>
2016-04-03 05:53:56 +0700

    fix motion scott

--- core/src/main/java/spark/game01/core/Gameplay00.java
+++ core/src/main/java/spark/game01/core/Gameplay00.java
@@ -14,13 +14,18 @@ public class Gameplay00 extends Screen{
 
   private final ScreenStack ss;
   private final ImageLayer bg;
-    private final ImageLayer backbutton;
-    private Scott scott;
+  private final ImageLayer gndfight;
+  private final ImageLayer backbutton;
+  private Scott scott;
 
   public Gameplay00(final ScreenStack ss) {
-    this.ss = ss;
+      this.ss = ss;
       Image bgImage = assets().getImage("images/screen00.png");
       this.bg = graphics().createImageLayer(bgImage);
+
+      Image gndfightImage = assets().getImage("images/gndfight.png");
+      this.gndfight = graphics().createImageLayer(gndfightImage);
+      gndfight.setTranslation(0,360);
 
       Image backImage = assets().getImage("images/backbutton.png");
       this.backbutton = graphics().createImageLayer(backImage);
@@ -41,9 +46,10 @@ public class Gameplay00 extends Screen{
     super.wasShown();
     this.layer.add(bg);
     this.layer.add(backbutton);
+    this.layer.add(gndfight);
 
 
-      scott = new Scott(320f,400f);
+      scott = new Scott(320f,360f);
       this.layer.add(scott.layer());
 
 
